be7cef5fb610c096c7c8ca353b3693f579f157bc
Gerry Agbobada <gagbobada+git@gmail.com>
2020-05-26 16:01:19 +0200

    Better defensive programming for secrets

--- bar_themes/gorgehousse.lua
+++ bar_themes/gorgehousse.lua
@@ -19,7 +19,10 @@ local batteryarc_widget = require("awesome-wm-widgets.batteryarc-widget.batterya
 local net_widgets = require("net_widgets")
 
 local config = require("../config")
-local secrets = require("secrets") or require("secrets-example")
+local secrets_available, secrets = pcall(require, "secrets")
+if (not secrets_available) then
+    secrets = require("secrets-example")
+end
 
 -- * Widget definitions
 -- ** Temp widget
